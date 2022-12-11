9759a923720e03599342e2327a40c24c9ca284e3
Ivanco <ivan.wasilev21@gmail.com>
2022-12-11 14:04:03 +0300

    11.12.22

---
+++ cars-app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+constructor(private fb: FormBuilder ){}
+
+  goScroll(target:HTMLElement) {
+    target.scrollIntoView({behavior: 'smooth'});
+  }
+}
